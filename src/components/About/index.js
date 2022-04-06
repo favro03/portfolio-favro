9c2d7f4478b36f50048dd5ea33efeebc34bbd3da
tfavro <wetr9902@gmail.com>
2022-04-05 22:09:32 -0500

    About component added

---
+++ src/components/About/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import heroImage from "../../assets/cover/hero.jpg";
+
+function About(){
+
+    return(
+        <section className="my-5">
+            <h1>About</h1>
+            <img src={heroImage} className="my-2" style={{ width: "100%" }} alt="cover"  />
+            <div className="my-2">
+                    <p>
+                    I am a Information Security professional with nine years industry experience and thirteen years project management experience. 
+                    Highly skilled in risk assessment, risk management, and risk analysis. <br></br><br></br>
+                    
+                    Knowledge of industry standard risk analysis tools and methodology. Pre and post data breach experience. <br></br>
+                    Skills include C++, Python, git, HTML, css, and JavaScript. 
+                    
+                </p>
+            </div>
+        </section>
+    )
+}
+export default About;
